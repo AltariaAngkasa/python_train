115889f7fd7a8a68181f0d1c506e7168eec96bd4
Alif Wilah Desfitrawan <36957119+Alifwilah@users.noreply.github.com>
2022-10-28 19:12:10 +0700

    Create konversiJamMenitDetik.py

---
+++ konversiJamMenitDetik.py
@@ -0,0 +1,8 @@
+jam = int(input("Masukkan satuan jam: "))
+menit = int(input("Masukkan satuan menit: "))
+
+CdetikJam = jam * 3600
+CdetikMenit = menit * 60
+
+print(f"Pada {jam} jam, ada {CdetikJam} detik")
+print(f"pada {menit} menit, ada {CdetikMenit} detik")
